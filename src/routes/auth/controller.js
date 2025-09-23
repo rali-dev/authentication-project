f2707841ac0b0c9c5e8527058336be43ae40c95f
rahimi.ali <ali.saee@outlook.de>
2025-09-24 01:13:03 +0200

    4.nodejs - register

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,9 +1,27 @@
 const controller = require('./../controller');
+const _= require('lodash');
+const bcrypt = require('bcrypt');
 
 module.exports = new (class extends controller {
   async register(req, res){
-    // console.log(this);
-    res.send('register');
+   let user =  await this.User.findOne({email: req.body.email});
+   if(user) {
+     return this.response({
+      res, code: 400, message: 'this user already registered '
+     });
+   }
+  //  const {email, name, password} = req.body;
+  //  user = new this.User({email, name, password}).save();
+  user = new this.User(_.pick(req.body, ['email', 'name', 'password']));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  this.response({
+    res, message: 'the user registered successfully',
+    data: _.pick(user, ['_id', 'email', 'name'])
+  });
   }
 
   async login(req, res){
