51796883dead4e3b13249200ba628d9242f900c8
rahimi.ali <ali.saee@outlook.de>
2025-12-28 22:11:33 +0100

    10.nodejs - handling errors outside Express route handlers in Node.js

--- index.js
+++ index.js
@@ -18,8 +18,29 @@ mongoose
   .then(() => debug('Connected to MongoDB...'))
   .catch((err) => debug('Could not connect to MongoDB...', err));
 
+// Handles unexpected errors thrown synchronously in the application
+process.on('uncaughtException', (ex) => {
+  console.log('Uncaught Exception');
+  winston.error(ex.message, ex);
+});
+
+// Handles unhandled promise rejections (asynchronous errors)
+process.on('unhandledRejection', (ex) => {
+  console.log('Unhandled Rejection');
+  winston.error(ex.message, ex);
+});
+
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 
+
+/* 
+// Test unhandled rejection
+const p = Promise.reject(new Error('Something failed outside'));
+p.then(() => console.log('Done'));
+
+// Test uncaught exception
+throw new Error('Something failed outside'); 
+*/
 app.use('/api', router);
 
 const port = process.env.PORT || 3000;
